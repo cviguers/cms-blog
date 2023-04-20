f6d3b29cd1730a889bf91167c539248f45fff733
cviguers <cviguers@gmail.com>
2023-04-19 20:02:00 -0400

    initial handlebars set up

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { Region, Product } = require('../models');
+// Import the custom middleware
+const withAuth = require('../utils/auth');
+
+// GET response for homepage handlebars
+router.get('/', async (req, res) => {
+  try {
+    // render homepage handlebar if logged in
+    res.render('home', {
+    });
+    // if err, throw err
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
