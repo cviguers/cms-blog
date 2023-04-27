26e9ea964bbef7dd1540d4b1eed5a8b70f40cd42
cviguers <cviguers@gmail.com>
2023-04-27 09:13:55 -0400

    all crud operating in tandem with fetchs

--- seeds/blogpostData.js
+++ seeds/blogpostData.js
@@ -3,8 +3,18 @@ const { Blogpost } = require('../models');
 const blogpostdata = [
 
   {
-    title: 'Winter Home',
-    content: 'Log cabin blanketed in heavy white snow with tall snow covered pine trees in the background.',
+    title: 'Fire and Ice by Robert Frost',
+    content: 'Some say the world will end in fire, Some say in ice. From what Ive tasted of desire I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate To know that for destruction ice Is also great, And would suffice.',
+  },
+
+  {
+    title: 'Hope by Emily Dickinson',
+    content:'HOPE is the thing with feathers That perches in the soul, And sings the tune without the words, And never stops at all, And sweetest in the gale is heard; And sore must be the storm That could abash the little bird That kept so many warm. I’ve heard it in the chillest land, And on the strangest sea; Yet, never, in extremity, It asked a crumb of me.',
+  },
+
+  {
+    title: 'Do Not Stand at My Grave and Weep by Mary Elizabeth Frye',
+    content: 'Do not stand at my grave and weep: I am not there; I do not sleep. I am a thousand winds that blow, I am the diamond glints on snow, I am the sun on ripened grain, I am the gentle autumn rain. When you awaken in the morning’s hush I am the swift uplifting rush Of quiet birds in circling flight. I am the soft starshine at night. Do not stand at my grave and cry: I am not there; I did not die.',
   },
 ];
 
